be5737632fa9dceebe865558383b2a9e58688bb3
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-01-20 05:15:30 +0530

    reverse an array

--- pratice.cpp
+++ pratice.cpp
@@ -4,5 +4,24 @@ using namespace std;
 int main(){
 
   //Reverse of an array 
+  class Solution {
+  public:
+    void reverseArray(vector<int> &arr) {
+        
+        int n = arr.size();
+       
+        int end = n-1;
+        
+        for(int st=0;st<end;st++){
+            
+           swap(arr[st],arr[end]);
+        
+           end--;
+        }
+        
+        
+        
+    }
+};
   
 }
