ca761e04ea1583c34a9105b732bcca5a37e3c53c
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-01-22 23:51:26 +0530

    second max element

--- pratice.cpp
+++ pratice.cpp
@@ -4,7 +4,8 @@ using namespace std;
 int main(){
 
   //Reverse of an array 
-  class Solution {
+
+  /* class Solution {
   public:
     void reverseArray(vector<int> &arr) {
         
@@ -22,6 +23,41 @@ int main(){
         
         
     }
-};
-  
+};    
+
+*/
+
+// Secong maximum
+// class Solution {
+// public:
+//     int getSecondLargest(vector<int> &arr) {
+//         int ans = INT_MIN;
+//         int n = arr.size();
+
+//         // first loop → find largest
+//         for(int i = 0; i < n; i++){
+//             if(arr[i] > ans)
+//                 ans = arr[i];
+//         }
+
+//         int maxVal = ans;   // store largest temporarily (required logically)
+
+//         ans = INT_MIN;      // reuse ans for second largest
+
+//         // second loop → find second largest
+//         for(int i = 0; i < n; i++){
+//             if(arr[i] != maxVal && arr[i] > ans){
+//                 ans = arr[i];
+//             }
+//         }
+
+//         if(ans == INT_MIN) return -1;
+//         return ans;
+//     }
+// };
+
+
+
+
+
 }
