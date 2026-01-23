00287c0865f73e741da84cf903518255956af547
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-01-23 23:14:08 +0530

    next commmit

--- binary.cpp
+++ binary.cpp
@@ -29,35 +29,35 @@ int main() {
 
 // optimised code for binary search
 
-#include <iostream>
-using namespace std;
+// #include <iostream>
+// using namespace std;
 
-int binarySearch(int arr[], int n, int key)
-{
-  int low = 0, high = n - 1;
+// int binarySearch(int arr[], int n, int key)
+// {
+//   int low = 0, high = n - 1;
 
-  while (low <= high)
-  {
-    int mid = low + (high - low) / 2; // avoids overflow
+//   while (low <= high)
+//   {
+//     int mid = low + (high - low) / 2; // avoids overflow
 
-    if (arr[mid] == key)
-      return mid;
-    else if (arr[mid] < key)
-      low = mid + 1;
-    else
-      high = mid - 1;
-  }
-  return -1; // not found
-}
+//     if (arr[mid] == key)
+//       return mid;
+//     else if (arr[mid] < key)
+//       low = mid + 1;
+//     else
+//       high = mid - 1;
+//   }
+//   return -1; // not found
+// }
 
-int main()
-{
-  int arr[] = {10, 20, 30, 40, 50, 60};
-  int n = sizeof(arr) / sizeof(arr[0]);
-  int key = 40;
+// int main()
+// {
+//   int arr[] = {10, 20, 30, 40, 50, 60};
+//   int n = sizeof(arr) / sizeof(arr[0]);
+//   int key = 40;
 
-  int index = binarySearch(arr, n, key);
+//   int index = binarySearch(arr, n, key);
 
-  if (index != -1)
-    cout << "Found at index ";
-}
+//   if (index != -1)
+//     cout << "Found at index ";
+// }
