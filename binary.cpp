fbd24f32a0b8a68dc35df00406603c55cef5701b
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-01-25 23:58:18 +0530

    insert position

--- binary.cpp
+++ binary.cpp
@@ -129,46 +129,66 @@ class Solution {
 // };
 // }
 
+// class Solution {
+// public:
+//     vector<int> searchRange(vector<int>& arr, int k) {
+//         // First Occurence
+//         int st = 0, end = arr.size() - 1, first = -1, last = -1;
+
+//         while (st <= end) {
+//             int mid = st + (end - st) / 2;
+//             if (arr[mid] == k) {
+//                 first = mid;
+//                 end = mid - 1;
+
+//             } else if (arr[mid] < k) {
+//                 st = mid + 1;
+//             } else {
+//                 end = mid - 1;
+//             }
+//         }
+//          // Last Occurence
+
+//         st = 0, end = arr.size() - 1;
+//         while (st <= end) {
+//             int mid = st + (end - st) / 2;
+
+//             if (arr[mid] == k) {
+//                 last = mid;
+//                 st = mid + 1;
+//             } else if (arr[mid] < k) {
+//                 st = mid + 1;
+
+//             } else {
+//                 end = mid - 1;
+//             }
+//         }
+
+//         vector<int>a(2);
+//         a[0]=first;
+//         a[1]=last;
+
+//         return a;
+//     } 
+    
+// };
+
+
+//insert position
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& arr, int k) {
-        // First Occurence
-        int st = 0, end = arr.size() - 1, first = -1, last = -1;
+    int searchInsert(vector<int>& arr, int t) {
+        // int st = 0,end=nums.size()-1;
 
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-            if (arr[mid] == k) {
-                first = mid;
-                end = mid - 1;
+        int n = arr.size() - 1;
 
-            } else if (arr[mid] < k) {
-                st = mid + 1;
-            } else {
-                end = mid - 1;
-            }
-        }
-         // Last Occurence
+        for (int i = 0; i <= n; i++) {
+            if (arr[i] == t) {
+                return i;
+            } else if (arr[i] > t) {
+                return i;
 
-        st = 0, end = arr.size() - 1;
-        while (st <= end) {
-            int mid = st + (end - st) / 2;
-
-            if (arr[mid] == k) {
-                last = mid;
-                st = mid + 1;
-            } else if (arr[mid] < k) {
-                st = mid + 1;
-
-            } else {
-                end = mid - 1;
-            }
-        }
-
-        vector<int>a(2);
-        a[0]=first;
-        a[1]=last;
-
-        return a;
-    } 
-    
+            } 
+        }  return n+1;
+    }
 };
