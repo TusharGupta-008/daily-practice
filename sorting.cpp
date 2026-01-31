ff70dd3812a1c3042f37176354c6f8b9ab7d5cd1
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-01-31 21:00:01 +0530

    third sorting algo

--- sorting.cpp
+++ sorting.cpp
@@ -17,16 +17,16 @@ using namespace std;
 // }
 
 
-void insertionSort(int arr[], int n) {
-    for(int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
+// void insertionSort(int arr[], int n) {
+//     for(int i = 1; i < n; i++) {
+//         int key = arr[i];
+//         int j = i - 1;
 
-        while(j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
+//         while(j >= 0 && arr[j] > key) {
+//             arr[j + 1] = arr[j];
+//             j--;
+//         }
 
-        arr[j + 1] = key;
-    }
-}
+//         arr[j + 1] = key;
+//     }
+// }
