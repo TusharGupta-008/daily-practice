7c174f14840a867f7f883574a5da1d24ee5d7b25
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-01-31 20:58:36 +0530

    writting insertion sort

--- sorting.cpp
+++ sorting.cpp
@@ -2,16 +2,18 @@
 #include <vector>
 using namespace std;
 
-void selectionSort(int arr[], int n) {
-    for(int i = 0; i < n - 1; i++) {
-        int minIndex = i;
+// void selectionSort(int arr[], int n) {
+//     for(int i = 0; i < n - 1; i++) {
+//         int minIndex = i;
 
-        for(int j = i + 1; j < n; j++) {
-            if(arr[j] < arr[minIndex]) {
-                minIndex = j;
-            }
-        }
+//         for(int j = i + 1; j < n; j++) {
+//             if(arr[j] < arr[minIndex]) {
+//                 minIndex = j;
+//             }
+//         }
 
-        swap(arr[i], arr[minIndex]);
-    }
-}
+//         swap(arr[i], arr[minIndex]);
+//     }
+// }
+
+
