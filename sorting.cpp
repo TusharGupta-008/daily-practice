8d0f2a752b803b48852620689e25ef08cdfa7ee9
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-01 22:11:18 +0530

    merge sort

--- sorting.cpp
+++ sorting.cpp
@@ -30,4 +30,26 @@ using namespace std;
 //         arr[j + 1] = key;
 //     }
 // }
+void merge(vector<int>& a, int l, int m, int r) {
+    vector<int> temp;
+    int i = l, j = m + 1;
+
+    while (i <= m && j <= r) {
+        if (a[i] <= a[j]) temp.push_back(a[i++]);
+        else temp.push_back(a[j++]);
+    }
+    while (i <= m) temp.push_back(a[i++]);
+    while (j <= r) temp.push_back(a[j++]);
+
+    for (int k = l; k <= r; k++)
+        a[k] = temp[k - l];
+}
+
+void mergeSort(vector<int>& a, int l, int r) {
+    if (l >= r) return;
+    int m = (l + r) / 2;
+    mergeSort(a, l, m);
+    mergeSort(a, m + 1, r);
+    merge(a, l, m, r);
+}
 
