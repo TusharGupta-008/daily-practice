1e1e18171b3bfcdd782882ca5a9198ad27f6f1ef
TusharGupta-008 <tushargupta.dsce@gmail.com>
2026-02-02 23:53:04 +0530

    fibonacci using recursion

--- sorting.cpp
+++ sorting.cpp
@@ -65,3 +65,20 @@ using namespace std;
 //     printN(5);
 // }
 
+#include <iostream>
+using namespace std;
+
+int fib(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    return fib(n-1) + fib(n-2);
+}
+
+int main() {
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+
+    cout << "Fibonacci number = " << fib(n);
+    return 0;
+}
